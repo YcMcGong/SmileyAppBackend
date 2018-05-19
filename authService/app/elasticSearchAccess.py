3ee69d67ce8e853d965e10f1d1d1ffa1a5f2599e
Yicong M. Gong <gongyc3@gmail.com>
2018-05-19 00:20:50 -0700

    elastic search method added

--- authService/app/elasticSearchAccess.py
+++ authService/app/elasticSearchAccess.py
@@ -1,1 +1,98 @@
-# in progress
+"""
+____________________________________________________
+ Copyright 2018 Yicong Gong
+ All rights reserved, for demostration purpose only.
+____________________________________________________
+This file provides elastic search capability.
+
+"""
+# 
+from datetime import datetime
+from elasticsearch import Elasticsearch, RequestsHttpConnection
+from requests_aws4auth import AWS4Auth
+from config import aws_access_key_id, aws_secret_access_key
+
+awsauth = AWS4Auth(aws_access_key_id, aws_secret_access_key, 'us-west-1', 'es')
+host = 'search-mysmileapp-qebinjxpqr32k7pdq3edignhiu.us-west-1.es.amazonaws.com'
+
+es = Elasticsearch(
+    hosts=[{'host': host, 'port': 443}],
+    http_auth=awsauth,
+    use_ssl=True,
+    verify_certs=True,
+    connection_class=RequestsHttpConnection
+)
+
+DEFAULT_USER_INDEX = 'user-index-1'
+USER_TYPE = 'user'
+
+# index a document
+def index_document(index, type, body, id = None):
+    response = es.index(index=index, doc_type=type, id=id, body=doc)
+    return response['result']
+
+# index a user
+def user_index(user_ID, userData):
+    return index_document(DEFAULT_USER_INDEX, USER_TYPE, body = userData, id = user_ID)
+
+# get a user by user_ID
+def user_get(user_ID):
+    response = es.get(index=DEFAULT_USER_INDEX, doc_type=USER_TYPE, id=user_ID)
+
+def user_search(label):
+    response = es.search(index=DEFAULT_USER_INDEX, body= {"query":
+    {
+        "bool" : {
+            "should" : [
+            {
+                "prefix" : {
+                "user_ID" : label
+                }
+            },
+            {
+                "match_phrase_prefix" : {
+                "username" : label
+                }
+            },
+            {
+                "match_phrase_prefix" : {
+                "fullName" : label
+                }
+            },
+                        {
+                "match_phrase_prefix" : {
+                "firstName" : label
+                }
+            },
+                        {
+                "match_phrase_prefix" : {
+                "email" : label
+                }
+            }
+            ]
+        }
+        }})
+    if response['hits']['total']:
+        entryListFinal = []
+        entryList = response['hits']['hits']
+        for entry in entryList:
+            entryListFinal.append(entry['_source'])
+        return entryListFinal
+    else:
+        return False
+
+if __name__ == '__main__':
+    user_ID = 't12342'
+
+    doc = {
+        'email': 'test2@gmail.com',
+        'lastName': 'Kim',
+        'firstName': 'Kate2',
+        'fullName': 'Kate2 Kim',
+        'user_ID': 't12342',
+        'username': 'theBestK2'
+    }
+
+    user_index(user_ID, userData = doc)
+
+    print(user_search('Kate'))
--- authService/app/main.py
+++ authService/app/main.py
@@ -1,23 +1,58 @@
-from flask import Flask
+"""
+____________________________________________________
+ Copyright 2018 Yicong Gong
+ All rights reserved, for demostration purpose only.
+____________________________________________________
+"""
+from flask import Flask, render_template, request, redirect, url_for, flash, current_app, jsonify
 app = Flask(__name__)
 
 from utilities import status_response
 import dataAccess
 
+"""
+#_______________________
+# Health Check          |
+#_______________________|
+"""
 @app.route("/")
 def healthCheck():
     return "authService is up and running"
 
+"""
+#_______________________
+# User sign up          |
+#_______________________|
+"""
 @app.route("/user_sign_up", methods = ['POST'])
 def user_sign_up():
+    status = status_response()
     if request.method == 'POST':
         firstName = request.form.get('firstName')
         lastName = request.form.get('lastName')
         email = request.form.get('email')
         username = request.form.get('username')
         password = request.form.get('password')
-        dataAccess.user_sign_up(email, password, firstName, lastName, username)
+        if firstName and lastName and email and username and password:
+            try: 
+                dataAccess.lookup_email(email)
+                status.set_errorMessage('email already exist')
+                return status.get_response()
+            except:
+                try: 
+                    dataAccess.lookup_username(username)
+                    status.set_errorMessage('username already exist')
+                    return status.get_response()
+                except:
+                    response = dataAccess.user_sign_up(email, password, firstName, lastName, username)
+                    status.attach_data('data', response, isSuccess=True)
+    return status.get_response()
 
+"""
+#___________________________
+# User authenticate         |
+#___________________________|
+"""
 @app.route("/user_authenticate", methods = ['GET'])
 def user_authenticate():
     status = status_response()
@@ -26,22 +61,32 @@ def user_authenticate():
         email = request.args.get('email')
         username = request.args.get('username')
         password = request.args.get('password')
-        response = dataAccess.user_authenticate(user_ID=user_ID, email=email, username=username, password=password)
-        if response:
-            status.set_status = True
+        try:
+            response = dataAccess.user_authenticate(user_ID=user_ID, email=email, username=username, password=password)
+            status.attach_data('data', response, isSuccess=True)
+        except:
+            status.set_errorMessage('email or username not exist')
     return status.get_response()
 
+"""
+#_______________________
+#   Get user data       |
+#_______________________|
+"""
 @app.route("/user_get_data", methods = ['GET'])
 def user_get_data():
     status = status_response()
     if request.method == 'GET':
         user_ID = request.args.get('user_ID')
         dataType = request.args.get('dataType')
-        if dataType:
-            response = dataAccess.user_get(user_ID, dataType=dataType)
-        else:
-            response = dataAccess.user_get_basic(user_ID)
-        status.attach_data('data', response, isSuccess=True)
+        try:
+            if dataType:
+                response = dataAccess.user_get(user_ID, dataType=dataType)
+            else:
+                response = dataAccess.user_get_basic(user_ID)
+            status.attach_data('data', response, isSuccess=True)
+        except:
+            status.set_errorMessage('user_ID not exist')
     return status.get_response()
 
 
